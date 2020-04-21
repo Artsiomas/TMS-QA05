c736dbd3e6e5b3060a2e66dfb06f2b7361316e34
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 22:17:20 +0300

    Update task_6.java

--- src/HW_2/task_6.java
+++ src/HW_2/task_6.java
@@ -1,7 +1,10 @@
 package HW_2;
 
+// названия пакетов с маленькой буквы
+// task_6 -> Task_6
 public class task_6 {
     public static void main(String[] args) {
+        // 101 - в переменную
         for (int numbers = 2; numbers <101; numbers = numbers +2) {
             System.out.println("Число " + numbers);
         }
