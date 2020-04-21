83c54a93aa09db098807d82e3acf925c2c3c24a6
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 22:13:49 +0300

    Update task_2.java

--- src/HW_2/task_2.java
+++ src/HW_2/task_2.java
@@ -10,6 +10,6 @@ public class task_2 {
         for ( jellyFish=1,ours=3;ours<25; jellyFish*=2, ours= ours+3) {
                 System.out.println("Количество медуз  " + jellyFish + " через " + ours + " часов ");
         }
-
+// пустая строка не нужна
     }
 }
