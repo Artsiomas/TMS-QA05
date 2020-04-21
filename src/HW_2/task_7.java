90c2c98e5fb364c070a31b3dd321f4f0d5636c77
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 22:18:18 +0300

    Update task_7.java

--- src/HW_2/task_7.java
+++ src/HW_2/task_7.java
@@ -1,9 +1,13 @@
 package HW_2;
 
+// названия пакетов с маленькой буквы
+// task_7 -> Task_7
 public class task_7 {
     public static void main(String[] args) {
 
         int number, sum;
+        // int sum = 0;
+        // for (int number = 1; number <100; number = number +2) {тело цикла}
         for ( number = 1, sum = 0; number <100; number = number +2, sum =sum + number );
         System.out.println("Сумма: " + sum);
     }
