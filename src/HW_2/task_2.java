4e3f134f6e5b8287858013ae3a9cf55a7375559f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 22:13:15 +0300

    Update task_2.java

--- src/HW_2/task_2.java
+++ src/HW_2/task_2.java
@@ -1,8 +1,12 @@
 package HW_2;
 
+// названия пакетов с маленькой буквы
+// task_2 -> Task_2
 public class task_2 {
     public static void main(String[] args) {
-        int jellyFish, ours;
+        int jellyFish, ours; // переменные в 2 строки
+        // for (int jellyFish=1,ours=3;ours<25; jellyFish*=2, ours= ours+3) {
+        // ours => hours
         for ( jellyFish=1,ours=3;ours<25; jellyFish*=2, ours= ours+3) {
                 System.out.println("Количество медуз  " + jellyFish + " через " + ours + " часов ");
         }
