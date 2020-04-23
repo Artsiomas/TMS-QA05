c7b53fc08a108d1c273e233b51ab90f95bf4031f
Artsiom Zagvozdin <zagvozdin.artsiom@gmail.com>
2020-04-23 15:52:09 +0300

    HW_3

---
+++ src/HW_3/Task_7.java
@@ -0,0 +1,28 @@
+package HW_3;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        int num [] = new int [] {95,56,-45,9,-63,59,40,47,-96,-3};
+        int a, b, t;
+        int size;
+        size = num.length;
+        System.out.print("Исходный массив: ");
+        for (int i=0; i < size; i++)
+            System.out.print(" " + num [i]);
+            System.out.println();
+
+        for (a=1; a < size; a++)
+            for (b=size-1; b>=a; b--) {
+                if (num[b - 1] > num [b]) {
+                    t = num [b-1];
+                    num [b-1] = num[b];
+                    num [b] = t;
+                }
+            }
+
+        System.out.print("Отсортированный массив: ");
+        for (int i=0; i < size; i++)
+            System.out.print(" " + num [i]);
+
+    }
+}
