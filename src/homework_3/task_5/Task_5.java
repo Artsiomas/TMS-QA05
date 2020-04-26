ecad0ee97c5656ce286830a625620a55aa6bd404
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-26 23:20:41 +0300

    Update Task_5.java

--- src/homework_3/task_5/Task_5.java
+++ src/homework_3/task_5/Task_5.java
@@ -3,6 +3,7 @@ package homework_3.task_5;
 // не забывай форматировать код
 public class Task_5 {
     public static void main(String[] args) {
+        // int num[] = {10,20,30,40};
         int num[] = new int [] {10,20,30,40};
         int size = num.length;
         int a = size - 1;
