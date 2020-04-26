f115fe0aecd11a86e61590f90cd6166b8f955fa5
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-26 23:18:41 +0300

    Update Task_6.java

--- src/homework_3/task_6/Task_6.java
+++ src/homework_3/task_6/Task_6.java
@@ -2,18 +2,21 @@ package homework_3.task_6;
 
 import java.util.Arrays;
 
+// не забывай форматировать код
 public class Task_6 {
     public static void main(String[] args) {
         int num[] = new int[]{95, 56, 45, 9, 63, 59, 40, 47, 96, 3};
 
-        int a = 0;
+        int a = 0; // плохое название переменной
+        // старайся выносить логические блоки кода в отдельные методы
         for (int i = 0; i < num.length; i++) {
             if (num[i]%2 == 0) {
                 a++;
             }
         }
-        int[] num_2 = new int[a];
+        int[] num_2 = new int[a]; // плохое название переменной
         int index = 0;
+        // старайся выносить логические блоки кода в отдельные методы
         for (int i = 0; i < num.length; i++) {
             if (num[i]%2 == 0) {
                 num_2[index] = num[i];
