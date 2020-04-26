3f5b28b72be17bbe423a80ae9d8239fe27be145d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-26 23:07:36 +0300

    Update Task_3.java

--- src/homework_3/task_3/Task_3.java
+++ src/homework_3/task_3/Task_3.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Task_3 {
     public static void main(String[] args) {
+        // int[] num = {66,56,89,3,26,58,44,92};
         int [] num = new int [] {66,56,89,3,26,58,44,92};
         int size = num.length;
         for (int i = 0; i < size; i++) {
-            if (i/2 == 1) {
+            if (i/2 == 1) { // i % 2 != 0
                 num[i] = 0;
             }
         }
