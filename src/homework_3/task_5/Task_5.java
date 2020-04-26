eb104548e02bce4a2cbe79e125b32c5ec74be572
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-26 23:17:12 +0300

    Update Task_5.java

--- src/homework_3/task_5/Task_5.java
+++ src/homework_3/task_5/Task_5.java
@@ -1,17 +1,20 @@
 package homework_3.task_5;
 
+// не забывай форматировать код
 public class Task_5 {
     public static void main(String[] args) {
         int num[] = new int [] {10,20,30,40};
         int size = num.length;
         int a = size - 1;
         int i;
+        // старайся выносить логические блоки кода в отдельные методы
         for ( i = 0; i < size/2; i++) {
             int buffer = num[i];
             num[i] = num [a];
             num[a] = buffer;
             a--;
         }
+        // старайся выносить логические блоки кода в отдельные методы
         for ( i = 0; i<size; i++) {
             System.out.print( num[i] + " ");
         }
