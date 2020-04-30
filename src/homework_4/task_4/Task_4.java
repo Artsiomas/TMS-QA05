25b88be7c0031f337e41518a913bf1b2b62f851c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-30 21:27:46 +0300

    Update Task_4.java

--- src/homework_4/task_4/Task_4.java
+++ src/homework_4/task_4/Task_4.java
@@ -2,12 +2,13 @@ package homework_4.task_4;
 
 import homework_4.task_1.MyClass;
 
+// форматируй код
 public class Task_4 {
     public static void main(String[] args) {
 
  MyClass human = new MyClass ();
 
-        MyClass human_1 = new MyClass ();
+        MyClass human_1 = new MyClass (); // названия переменных так себе
         MyClass human_2 = new MyClass ();
         MyClass human_3 = new MyClass ();
 
