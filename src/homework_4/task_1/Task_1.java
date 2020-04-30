ae2b512f44ae2cd032f211be6c6aff1e1ead16a9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-30 21:19:09 +0300

    Update Task_1.java

--- src/homework_4/task_1/Task_1.java
+++ src/homework_4/task_1/Task_1.java
@@ -4,11 +4,11 @@ public class Task_1 {
     public static void main(String[] args) {
         MyClass Human = new MyClass();
 
-
+// пустая строка не нужна
         Human.getAge();
         Human.getName();
 
-
+// пустая строка не нужна
         Human.showName();
     }
 }
